73d2f144471b24e603ef63826253d2f9048bb1d7
ValuhkJv <valuhkjv@gmail.com>
2024-03-13 14:26:16 +0700

    ok
    ok

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\tes;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+route::get('/tes', [tes::class,'getdata'] );
